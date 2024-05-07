edaa2962159ce21ed3db54d8f14191fbd03541c9
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-07 14:56:14 +0300

    adding aws auth

--- aws-auth/main.tf
+++ aws-auth/main.tf
@@ -3,13 +3,13 @@ resource "helm_release" "aws_auth" {
   chart      = "./charts/aws-auth"
   
 
-  set {
-    name   = "accountId"
-    value = "${local.account_id}"
- }
-  set {
-    name   = "nodegroup_role_arn"
-    value = "[arn:aws:iam::${local.account_id}:role/node-group-role]"
- }
+#   set {
+#     name   = "accountId"
+#     value = "${local.account_id}"
+#  }
+#   set {
+#     name   = "nodegroup_role_arn"
+#     value = "[arn:aws:iam::${local.account_id}:role/node-group-role]"
+#  }
 
 }
