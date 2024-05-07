5d68d192aa3ad3a06624c932e46f1a84a1264079
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-07 15:49:04 +0300

    adding aws auth

--- aws-auth/main.tf
+++ aws-auth/main.tf
@@ -1,15 +1,38 @@
+resource "kubernetes_labels" "aws_auth_labels" {
+  api_version = "v1"
+  kind        = "ConfigMap"
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+  labels = {
+    "app.kubernetes.io/managed-by" = "Helm"
+  }
+}
+resource "kubernetes_annotations" "aws_auth_annotations" {
+  api_version = "v1"
+  kind        = "ConfigMap"
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+  annotations = {
+    "meta.helm.sh/release-name": "aws-auth"
+    "meta.helm.sh/release-namespace": "default"
+  }
+}
+
 resource "helm_release" "aws_auth" {
   name       = "aws-auth"
   chart      = "./charts/aws-auth"
   
-
-#   set {
-#     name   = "accountId"
-#     value = "${local.account_id}"
-#  }
-#   set {
-#     name   = "nodegroup_role_arn"
-#     value = "[arn:aws:iam::${local.account_id}:role/node-group-role]"
-#  }
-
+  
+  set {
+    name   = "accountId"
+    value = "${local.account_id}"
+  }
+  set_list {
+    name   = "nodegroup_role_arn"
+    value = ["arn:aws:iam::${local.account_id}:role/node-group-role"]
+  }
 }
--- aws-auth/provider.tf
+++ aws-auth/provider.tf
@@ -2,12 +2,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_info.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_info.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "kubectl"
-    args = ["delete", "cm", "aws-auth", "-n", "kube-system"]
-  }
 }
 
 provider "helm" {
@@ -15,10 +9,5 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks_info.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_info.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "kubectl"
-      args = ["delete", "cm", "aws-auth", "-n", "kube-system"]
-    }
   } 
 }
