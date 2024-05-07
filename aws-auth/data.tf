829eae3c689eb0dcac4e6080742c3a3df7c13b9e
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-07 14:42:13 +0300

    adding aws auth

--- aws-auth/data.tf
+++ aws-auth/data.tf
@@ -10,9 +10,9 @@ locals {
 
 
 data "aws_eks_cluster_auth" "eks" {
-  name = "eks_cluster"
+  name = var.cluster_name
 }
 
 data "aws_eks_cluster" "eks_info" {
-  name = "eks_cluster"
+  name = var.cluster_name
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "aws_auth" {
   source = "./aws-auth"
-  # cluster_name = "eks-cluster"
+  cluster_name = "eks_cluster"
 }
 
 
