5e4bd9140661d6e791cc9b0b512e26a3c6a22058
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-09 17:44:20 +0300

    add elb tag for elb controller deployment

--- alb/main.tf
+++ alb/main.tf
@@ -1,26 +1,25 @@
-# resource "helm_release" "aws-load-balancer-controller" {
-#   name       = "aws-load-balancer-controller"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "eks/aws-load-balancer-controller"
-#   version    = "1.7.2"
+resource "helm_release" "aws-load-balancer-controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "eks/aws-load-balancer-controller"
+  version    = "1.7.2"
+  values = [
+    "${file("values.yaml")}"
+  ]
 
-#   values = [
-#     "${file("values.yaml")}"
-#   ]
+  # set {
+  #   name  = "serviceAccount.annotations"
+  #   value = "true"
+  # }
 
-#   set {
-#     name  = "cluster.enabled"
-#     value = "true"
-#   }
+  # set_list {
+  #   name  = "serviceAccount.annotations"
+  #   value = "{}"
+  # }
 
-#   set {
-#     name  = "metrics.enabled"
-#     value = "true"
-#   }
-
-#   set {
-#     name  = "service.annotations.prometheus\\.io/port"
-#     value = "9127"
-#     type  = "string"
-#   }
-# }
+  # set {
+  #   name  = "service.annotations.prometheus\\.io/port"
+  #   value = "9127"
+  #   type  = "string"
+  # }
+}
