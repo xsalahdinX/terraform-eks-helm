04ff8ab3b2c18e8f7679553492ac973024f99d72
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-10 17:43:46 +0300

    add efs  controller deployment

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "aws_auth" {
-  source       = "./aws-auth"
-  cluster_name = "eks"
+  source          = "./aws-auth"
+  cluster_name    = "eks"
   iam_admin_roles = ["eks_access_role"]
-  nodegroup_role = ["eks_nodegroup_role"]
+  nodegroup_role  = ["eks_nodegroup_role"]
 
 }
 
 
 module "alb" {
-  source       = "./alb"
-  cluster_name = "eks"
-  version    = "1.7.1"
+  source            = "./alb"
+  cluster_name      = "eks"
+  elb_chart_version = "1.7.1"
 }
 
 
