5471e29c6520907f202b95c79a69597c44629737
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-10 13:46:44 +0300

    add elb tag for elb controller deployment

--- alb/main.tf
+++ alb/main.tf
@@ -10,7 +10,7 @@ resource "helm_release" "aws-load-balancer-controller" {
 
   set {
     name  = "serviceAccount.annotations"
-    value = "arn:aws:iam::${locals.account_id}:role/alb-ingress-controller"
+    value = "arn:aws:iam::${local.account_id}:role/alb-ingress-controller"
   }
 
   set {
