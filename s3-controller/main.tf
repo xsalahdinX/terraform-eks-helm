c555abe28c0cfda9a80914dde39a2982ceca1a31
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-20 13:48:55 +0300

    s3 contrller project  test

--- s3-controller/main.tf
+++ s3-controller/main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "kms_encrypted" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.id
+      kms_master_key_id = aws_kms_key.mykey.arn
       sse_algorithm     = "aws:kms"
     }
   }
