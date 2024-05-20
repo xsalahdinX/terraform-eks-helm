f250b69a26e4eefc1c306b04f8bfa8683a3967a6
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-20 11:41:30 +0300

    s3 contrller project  test

--- s3-controller/gatwayendpoit.tf
+++ s3-controller/gatwayendpoit.tf
@@ -1,47 +1,47 @@
-# resource "aws_vpc_endpoint" "s3" {
-#   vpc_id            = data.aws_vpc.eks-vpc.id
-#   service_name      = "com.amazonaws.${var.region}.s3"
-#   route_table_ids   = [data.aws_route_table.private-aws_route_table.id]
-#   vpc_endpoint_type = "Gateway"
-#   policy            = local.s3-endpoint-policy
-#   tags              = { Name = "app-vpce-s3" }
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = data.aws_vpc.eks-vpc.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  route_table_ids   = [data.aws_route_table.private-aws_route_table.id]
+  vpc_endpoint_type = "Gateway"
+  policy            = local.s3-endpoint-policy
+  tags              = { Name = "app-vpce-s3" }
 
-# }
+}
 
-# data "aws_subnets" "private_subnets" {
-#   filter {
-#     name   = "tag:Name"
-#     values = ["private_subnets_*"]
-#   }
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "tag:Name"
+    values = ["private_subnets_*"]
+  }
 
-# }
+}
 
-# data "aws_route_table" "private-aws_route_table" {
-#   subnet_id = data.aws_subnets.private_subnets.ids[0]
-# }
+data "aws_route_table" "private-aws_route_table" {
+  subnet_id = data.aws_subnets.private_subnets.ids[0]
+}
 
 
-# data "aws_vpc" "eks-vpc" {
-#   filter {
-#     name   = "tag:Name"
-#     values = ["EKS_VPC"]
-#   }
+data "aws_vpc" "eks-vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["EKS_VPC"]
+  }
 
-# }
+}
 
-# locals {
-#   s3-endpoint-policy = <<POLICY
-# {
-# 	"Version": "2012-10-17",
-# 	"Statement": [
-# 		{
-# 			"Sid": "AllowAllActionsToAllPrincipals",
-# 			"Effect": "Allow",
-# 			"Principal": "*",
-# 			"Action": "*",
-# 			"Resource": "*"
-# 		}
-# 	]
-# }
-# POLICY
-# }
+locals {
+  s3-endpoint-policy = <<POLICY
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "AllowAllActionsToAllPrincipals",
+			"Effect": "Allow",
+			"Principal": "*",
+			"Action": "*",
+			"Resource": "*"
+		}
+	]
+}
+POLICY
+}
