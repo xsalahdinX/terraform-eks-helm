059a3015cb142ec9edb20b56af884f6b44f36c5f
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-20 11:27:00 +0300

    s3 contrller project  test

--- s3-controller/iam.tf
+++ s3-controller/iam.tf
@@ -1,21 +1,21 @@
-resource "aws_kms_key" "mykey" {
-  description = "This key is used to encrypt bucket objects"
-}
+# resource "aws_kms_key" "mykey" {
+#   description = "This key is used to encrypt bucket objects"
+# }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.s3-bucket-name
-  tags   = { Name = "My bucket", Environment = "Dev" }
-}
+# resource "aws_s3_bucket" "s3_bucket" {
+#   bucket = var.s3-bucket-name
+#   tags   = { Name = "My bucket", Environment = "Dev" }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3-conifgration" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
+# resource "aws_s3_bucket_server_side_encryption_configuration" "s3-conifgration" {
+#   bucket = aws_s3_bucket.s3_bucket.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.mykey.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
 
 
 resource "aws_iam_policy" "bucket_policy" {
