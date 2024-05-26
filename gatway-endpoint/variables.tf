b22da54a4a95125603edb275e31b01d11f82ebb1
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-26 17:12:57 +0300

    some naming changes

--- gatway-endpoint/variables.tf
+++ gatway-endpoint/variables.tf
@@ -1,6 +1,6 @@
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket"
-  type = set(string)
+  type = string
 }
 
 
@@ -8,4 +8,9 @@ variable "region" {
   description = "The region of the S3 bucket"
   default     = "us-east-1"
 
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type = set(string)
 }
