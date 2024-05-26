47906bf244617247c54139f360430e733c4d6b7a
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-26 14:50:55 +0300

    some naming changes

--- gatway-endpoint/endpoit.tf
+++ gatway-endpoint/endpoit.tf
@@ -43,7 +43,7 @@ data "aws_iam_policy_document" "s3_endpoint_policy" {
       "s3:DeleteObject",
       "s3:ListMultipartUploadParts"
     ]
-    resources = ["*"]
+    resources = ["arn:aws:s3:::azzgamilsalahgg-s3-bucket"]
     principals {
       type        = "AWS"
       identifiers = ["*"]
