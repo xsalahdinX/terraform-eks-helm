c0edad6706317e0b6e070fc6ae6ab3d8f6a9f343
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-27 16:09:43 +0300

    adjust  gatway endpoint policy structure

--- main.tf
+++ main.tf
@@ -23,26 +23,26 @@ module "alb" {
 # }
 
 
-# module "test-s3" {
-#   source = "./test-s3"
-#   s3_bucket_name = "azzgamilsalahgg-s3-bucket"
+module "test-s3" {
+  source = "./test-s3"
+  s3_bucket_name = "azzgamilsalahgg-s3-bucket"
 
   
-# }
+}
 
-# module "s3-controller" {
-#   source = "./s3-controller"
-#   cluster_name = "eks"
-#   s3_bucket_name = ["azzgamilsalahgg-s3-bucket"]
-#   aws_kms_alias = "alias/test"
-#   depends_on = [ module.test-s3]
-# }
+module "s3-controller" {
+  source = "./s3-controller"
+  cluster_name = "eks"
+  s3_bucket_name = ["azzgamilsalahgg-s3-bucket"]
+  aws_kms_alias = "alias/test"
+  depends_on = [ module.test-s3]
+}
 
 
-#  module "gatway-s3" {
-#   source = "./gatway-endpoint"
-#   region = "us-east-1"
-#   bucket_name = ["azzgamilsalahgg-s3-bucket", "azzgamilsalahgg-s3-bucket2"]
-#   depends_on = [ module.s3-controller]
+ module "gatway-s3" {
+  source = "./gatway-endpoint"
+  region = "us-east-1"
+  bucket_name = ["azzgamilsalahgg-s3-bucket", "azzgamilsalahgg-s3-bucket2"]
+  depends_on = [ module.s3-controller]
 
-#  }
+ }
