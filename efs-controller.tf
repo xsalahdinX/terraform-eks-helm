e0e1d1a135bf01379f4cff7639b88e021b8949b7
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-05-31 15:40:22 +0300

    reform all modules

--- efs-controller.tf
+++ efs-controller.tf
@@ -1,5 +1,5 @@
 module "efs-controller" {
-  source = "./efs"
+  source = "./efs-controller"
   efs_controller_chart_version = "3.0.3"
   values_path              = "./values/efs-controller.yaml"
   efs_controller_namespace = "efs-controller"
@@ -7,7 +7,8 @@ module "efs-controller" {
   efs_role                 = "efs-controller-role"
   efs_storage_class_name   = "efs-sc"
   cluster_name             = local.cluster_name
-  efs_id                   = var.efs_id
-  eks_issuer_url           = var.eks_issuer_url
-  eks_issuer_arn           = var.eks_issuer_arn
+  efs_id                   = aws_efs_file_system.eks-file-system.id
+  eks_issuer_url           = local.eks_issuer_url
+  eks_issuer_arn           = local.eks_issuer_arn
+  account_id               = local.account_id
 }
