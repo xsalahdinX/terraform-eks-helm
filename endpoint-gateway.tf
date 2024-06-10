2bddb2e3fe9b76869ce734c4c2ce1a63be261791
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-06-10 16:06:20 +0300

    refine

--- endpoint-gateway.tf
+++ endpoint-gateway.tf
@@ -1,5 +1,5 @@
  module "gatway-s3" {
-  source = "github.com/xsalahdinX/terraform-modules//gatway-endpoint"
+  source = "github.com/xsalahdinX/terraform-modules//endpoint-gatway"
   region = "us-east-1"
   bucket_name = ["xsalahdinX-s3"]
   depends_on = [ module.test-s3]
